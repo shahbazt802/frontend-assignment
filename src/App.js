087be2919618f5f782b9425b81271f9619f48890
SHAHBAZ TAUHEED <45005775+shahbazt802@users.noreply.github.com>
2025-01-16 18:00:59 +0530

    added project table to show and css applied

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import "./App.css";
+import LandingPage from "./Page/LandingPage";
+import "./Styling/Custom.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <LandingPage />
     </div>
   );
 }
---
+++ src/Component/Formatter/formate.js
@@ -0,0 +1,10 @@
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
+};
+
+export const formatPercentage = (percentage) => {
+  return `${percentage.toFixed(2)}%`;
+};
